31dbbcd5e4d290cc3e89c2d15a48b90d70342f5e
Sammy <myallio94@gmail.com>
2025-03-25 06:33:07 +0000

    betty

--- data_functions.c
+++ data_functions.c
@@ -1,10 +1,22 @@
 #include "main.h"
 
+/**
+ * _putchar - prints a char
+ * @c: character
+ *
+ * Return: int
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+/**
+ * _printstring - prints a string
+ * @str: the string input
+ *
+ * Return: count
+ */
 int _printstring(char *str)
 {
 	int count = 0;
@@ -17,7 +29,14 @@ int _printstring(char *str)
 	return (count);
 }
 
-int _formatstring(char *str,int count)
+/**
+ * _formatstring - formats string for printf
+ * @str: the string input
+ * @count: count of string
+ *
+ * Return: _printstring func
+ */
+int _formatstring(char *str, int count)
 {
 	if (str == NULL)
 	{
