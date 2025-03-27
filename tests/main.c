6d003dcf85df68103fabe943a64b9a4935668b9b
Sammy Hill <myallio94@gmail.com>
2025-03-27 15:54:20 +1100

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -1,16 +1,29 @@
 #include "../main.h"
 #include <stdio.h>
 #include <limits.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    int len1, len2;
+    int len;
+    int len2;
 
-    len1 = _printf("%d\n", INT_MIN);
-    len2 = printf("%d\n", INT_MIN);
-
-    printf("_printf returned: %d\n", len1);
-    printf("printf returned: %d\n", len2);
-
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
     return (0);
 }
